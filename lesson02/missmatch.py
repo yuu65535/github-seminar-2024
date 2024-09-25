43dc05611c6c6c9d1e777d11a1dd21d8b936f22d
SATO Daisuke <densuke@fuga.jp>
2024-09-25 08:13:52 +0000

    lesson02(テスト追加)

--- lesson02/missmatch.py
+++ lesson02/missmatch.py
@@ -6,5 +6,13 @@ def add_one():
     pass  # ここにコードを書きます
 
 
+# リストの中身が全てint型であることを確認する関数
+def is_int_list(lst: list[any]) -> bool:
+    for item in lst:
+        if not isinstance(item, int):
+            return False
+    return True
+
+
 if __name__ == "__main__":
     pass  # ここにadd_one()を呼び出すコードを書く
