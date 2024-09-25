b9486d2bcc12df427657521518cd325cd213262e
SATO Daisuke <densuke@fuga.jp>
2024-09-25 08:16:16 +0000

    回答例除去

--- lesson02/missmatch.py
+++ lesson02/missmatch.py
@@ -1,18 +1,13 @@
 # 型のミスマッチが起こすバグの例
 
 
-def add_one(x: int) -> int:
-    """引数に1を足して返すだけの関数"""
-    return x + 1
+def add_one():
+    pass
 
 
-# リストの中身が全てint型であることを確認する関数
-def is_int_list(lst: list[any]) -> bool:
-    for item in lst:
-        if not isinstance(item, int):
-            return False
-    return True
+def is_int_list():
+    pass
 
 
 if __name__ == "__main__":
-    pass  # ここにadd_one()を呼び出すコードを書く
+    pass  # ここに上記関数を使うコードを書いてみる
